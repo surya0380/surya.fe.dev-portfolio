0032d0eeff19aee845b40365db2906838a22c570
surya0380 <surya0380@gmail.com>
2025-10-19 01:18:53 +0530

    fix: importing route from rrd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
