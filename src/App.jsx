3b98760fa9c8e1b78b005826daf8aab81a9e1dcd
surya0380 <surya0380@gmail.com>
2025-10-19 01:04:25 +0530

    feat: Initialize React portfolio project with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
