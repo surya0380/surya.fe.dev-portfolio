b84be5314a55063b533dfbb52a8f12846cb455a9
surya0380 <surya0380@gmail.com>
2025-10-19 18:38:14 +0530

    feat: added skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import StarBackground from '../components/StarBackground'
 import NavBar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
+import SkillSection from '../components/SkillSection'
 
 const Home = () => {
     return (
@@ -21,6 +22,7 @@ const Home = () => {
             <main>
                 <HeroSection />
                 <AboutSection />
+                <SkillSection />
             </main>
 
             {/* footer */}
