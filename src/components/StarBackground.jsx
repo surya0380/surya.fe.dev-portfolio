0b55b630bcfffcc8441c62a32ea44b985f52ee0f
surya0380 <surya0380@gmail.com>
2025-10-19 13:10:40 +0530

    feat: adding meteor animation

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -2,9 +2,17 @@ import React, { useEffect, useState } from 'react'
 
 const StarBackground = () => {
     const [stars, setStars] = useState([]);
+    const [meteors, setMeteors] = useState([]);
 
     useEffect(() => {
         generateStars();
+        generateMeteors();
+
+        const handleResize = () => {
+            generateStars();
+        }
+
+        window.addEventListener('resize', handleResize);
     }, [])
 
 
@@ -26,6 +34,24 @@ const StarBackground = () => {
         setStars(newStars);
     }
 
+    const generateMeteors = () => {
+        const numberOfMeteors = 4;
+        const newMeteors = [];
+
+        for (let i = 0; i < numberOfMeteors; i++) {
+            newMeteors.push({
+                id: i,
+                size: Math.random() * 2 + 1,
+                x: Math.random() * 100,
+                y: Math.random() * 20,
+                delay: Math.random() * 15,
+                animationDuration: Math.random() * 3 + 3,
+
+            })
+        }
+        setMeteors(newMeteors);
+    }
+
     return (
         <div className='fixed inset-0 overflow-hidden pointer-events-none z-0'>
 
@@ -37,6 +63,17 @@ const StarBackground = () => {
                     top: star.y + "%",
                     opacity: star.opacity,
                     animationDuration: star.animationDuration + "s",
+                }}></div>
+            )}
+
+            {meteors.map(meteor =>
+                <div key={meteor.id} className='meteor animate-meteor' style={{
+                    width: meteor.size * 50 + "px",
+                    height: meteor.size * 2 + "px",
+                    left: meteor.x + "%",
+                    top: meteor.y + "%",
+                    animationDelay: meteor.delay,
+                    animationDuration: meteor.animationDuration + "s",
                 }}></div>
             )}
 
