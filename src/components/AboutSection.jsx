40bfeb2a7740397ea9632b14384ddbbdb43fda4b
surya0380 <surya0380@gmail.com>
2025-10-20 14:44:43 +0530

    fix: download resume section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,11 @@ const AboutSection = () => {
                 Get in Touch
               </a>
 
-              <a href="" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" '>
+              <a
+                href="https://drive.google.com/file/d/1Gv0_eKYjrAKHS1nHxugvP-Y7M5CBFdRb/view"
+                className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" '
+                target='_blank'
+              >
                 Download CV
               </a>
             </div>
